e63f96adfcb9f7ff743d8600555279ec966c808d
qls051 <qls050100@naver.com>
2024-02-19 22:13:29 +0900

    commit

---
+++ C++ project/C++ PROJECT.cpp
@@ -0,0 +1,43 @@
+﻿// C++ PROJECT.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    // 간단한 출력
+    std::cout << "I like pizza!\n";
+    std::cout << "It;s really good!\n";
+    
+    // 선언
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+
+    // integer (whole number)
+    int age = 21;
+    int year = 2024;
+    int days = 7.5;
+
+    // double (number including decimal)
+    double price = 10.99;
+    double temperature = 25.1;
+
+    // single character
+    char grade = 'A';
+    char currency = '$';
+    
+    // boolean (ture or false)
+    bool student = true;
+
+    // string
+    std::string name = "Bro";
+    std::cout << name;
+    
+    
+    return 0;
+}
