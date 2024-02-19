11e19bcbe096f3b0ddb5302053bfa4fe9b6e9491
qls051 <qls050100@naver.com>
2024-02-19 19:10:27 +0900

    clone

---
+++ C++ project/C++ PROJECT/C++ PROJECT.cpp
@@ -0,0 +1,17 @@
+﻿// C++ PROJECT.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    /* std::cout << "I like pizza!\n";
+    std::cout << "It;s really good!\n"; 문자열 출력 */
+    int x;
+    x = 5;
+    
+
+    std::cout << x;
+    
+    return 0;
+}
