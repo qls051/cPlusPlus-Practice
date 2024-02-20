048e4c08108c067ee20d474867649261f6c962e6
qls051 <qls050100@naver.com>
2024-02-20 11:56:34 +0900

    commit

--- C++ project/C++ PROJECT/C++ PROJECT.cpp
+++ C++ project/C++ PROJECT/C++ PROJECT.cpp
@@ -5,13 +5,68 @@
 
 int main()
 {
-    /* std::cout << "I like pizza!\n";
-    std::cout << "It;s really good!\n"; 문자열 출력 */
-    int x;
-    x = 5;
-    
+#pragma region 기본 C++
 
-    std::cout << x;
+    // 간단한 출력
+    std::cout << "I like pizza!\n";
+    std::cout << "It;s really good!\n";
+
+    // 선언
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+
+    // integer (whole number)
+    int age = 21;
+    int year = 2024;
+    int days = 7.5;
+
+    // double (number including decimal)
+    double price = 10.99;
+    double temperature = 25.1;
+
+    // single character
+    char grade = 'A';
+    char currency = '$';
+
+    // boolean (ture or false)
+    bool student = true;
+
+    // string
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::string address = "123 fake st.";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << "years old";
+
+#pragma endregion
+#pragma region const
+
+    double pi = 3.14159;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    //std::cout << circumference << "cm";
+
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+#pragma endregion
+#pragma region Namespaces
+
+
+    
+#pragma endregion
+
+
     
     return 0;
 }
