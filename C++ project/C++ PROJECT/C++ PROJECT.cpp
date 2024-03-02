19be4126287280af548622a84871447429ecc9e0
qls051 <qls050100@naver.com>
2024-03-02 15:21:31 +0900

    fill an array with user input

--- C++ project/C++ PROJECT/C++ PROJECT.cpp
+++ C++ project/C++ PROJECT/C++ PROJECT.cpp
@@ -753,12 +753,37 @@ int main()
         cout << food << '\n';
     }*/
     
-    const int SIZE = 100;
+    /*const int SIZE = 100;
     string foods[SIZE];
 
     fill(foods, foods + SIZE, "pizza");
     for (string food : foods) {
         cout << food << '\n';
+    }*/
+
+#pragma endregion
+
+#pragma region fill an array with user input
+
+    string foods[5];
+    int size = sizeof(foods) / sizeof(foods[0]);
+    string temp;
+
+    for (int i = 0; i < size; i++) {
+        cout << "Enter a food you like or 'q' #" << i + 1<< ": ";
+        getline(cin, temp);
+        if (temp == "q") {
+            break;
+        }
+        else {
+            foods[i] = temp;
+        }
+    }
+
+    cout << "You like the following food:\n";
+
+    for (int i = 0; !foods[i].empty();i++) {
+        cout << foods[i] << '\n';
     }
 
 #pragma endregion
