92ffc5cccddff6342d3b86cd7c32a2d7fa7ad16b
qls051 <qls050100@naver.com>
2024-03-06 09:09:03 +0900

    Dynamic memory

--- C++ project/C++ PROJECT/C++ PROJECT.cpp
+++ C++ project/C++ PROJECT/C++ PROJECT.cpp
@@ -1006,6 +1006,38 @@ int main()
 
 #pragma endregion
 
+#pragma region Dynamic memory
+
+    /*int* pNum = NULL;
+
+    pNum = new int;
+
+    *pNum = 21;
+
+    cout << "address: " << pNum << '\n';
+    cout << "value: " << *pNum << '\n';
+
+    delete pNum;*/
+    char* pGrades = NULL;
+    int size;
+
+    cout << "How many grades to enter in?: ";
+    cin >> size;
+
+    pGrades = new char[size];
+
+    for (int i = 0; i < size; i++) {
+        cout << "Enter grade #" << i + 1 << ": ";
+        cin >> pGrades[i];
+    }
+    for (int i = 0; i < size; i++) {
+        cout << pGrades[i] << " ";
+    }
+
+    delete[] pGrades;
+
+#pragma endregion
+
 
 return 0;
 }
